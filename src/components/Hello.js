082ab7809ba8e985125f23dcbec3b56062363e70
Nikola <nikolaviki@hotmail.com>
2020-05-22 19:42:04 +0200

    Create element update

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 const Hello = () => {
 
-    return React.createElement('div', null, React.createElement('h1', null, 'Ovo je proba JSX'))
+    return React.createElement('div', { id: '1', className: 'divElement' }, React.createElement('h1', null, 'Ovo je proba JSX'))
 }
 export default Hello
